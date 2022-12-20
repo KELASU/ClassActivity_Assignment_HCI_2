6151d301cb138c4ebbe00195a49d9f404cf908f7
KELASU <techmaker24@gmail.com>
2022-12-20 15:27:48 +0700

    updated

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-// Add your answer here
+
+
+$(".lesson-icon").mouseenter(function(){
+    var changing = $(this).next().show("text-contents")
+    $(".lesson-icon").not(changing).removeClass("text-contents")
+})
+
+$(".lesson-icon").mouseleave(function(){
+    var change = $(this).next().hide("text-contents")
+    $(".lesson-icon").not(change).removeClass("text-contents")
+})
